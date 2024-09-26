9e7eb655de58e41664a9851971f266f1f2c7ac6e
Omofolawee <omolojufathia@gmail.com>
2024-09-26 06:42:54 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import image from "./images/diagram.svg";
+import check from "./images/checked.svg";
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="body">
+      <section class="sec">
+        <div class="container">
+          <div class="text">
+            <h1 class="title">Stay updated</h1>
+            <p>Join 60,000+ product managers receiving monthly updates on:</p>
+            <div class="checked">
+              <img src={check} alt="" />
+              <p>Product discovery and building what matters</p>
+            </div>
+            <div class="checked">
+              <img src={check} alt="" />
+              <p>Measuring to ensure updates are a success</p>
+            </div>
+            <div class="checked">
+              <img src={check} alt="" />
+              <p>And much more!</p>
+            </div>
+            <label for="" class="email">
+              Email address <br />
+              <input
+                type="text"
+                class="input"
+                placeholder="email@company.com"
+              />
+            </label>
+            <button>Subscribe to monthly newsletter</button>
+          </div>
+          <div class="image-container">
+            <img src={image} alt="" class="image" />
+          </div>
+        </div>
+      </section>
     </div>
   );
-}
+};
 
 export default App;
